d14216119e1f282fd5bdcbe5980ea684a1cf2a93
Sergei Vanin <barsabantuy@gmail.com>
2020-10-25 22:28:06 +0300

    del comment

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,7 +8,7 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/myDB?serverTimezone=Europe/Moscow&useSSL=false";
     private static final String DRIVER = "com.mysql.jdbc.Driver";
     private static final String USER = "admin";
-    private static final String PASSWORD = "vsegda17";
+    private static final String PASSWORD = "password";
 
     public static Connection getConnection() throws SQLException {
         try {
@@ -17,18 +17,5 @@ public class Util {
             e.printStackTrace();
         }
         return DriverManager.getConnection(URL, USER, PASSWORD);
-//        try {
-//            Class.forName(DRIVER);
-//            System.out.println("Driver is found");
-//            try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
-//                System.out.println("Success");
-//                return connection;
-//            } catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        return null;
     }
 }
